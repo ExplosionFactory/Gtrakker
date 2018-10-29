787492956c121db2e06a38e23e4af43de7a19fa1
seancsutton1 <seancsutton1@gmail.com>
2018-10-29 14:59:08 -0500

    adding script tags in index.html

--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+var express = require('express');
+const axios = require('axios')
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/../client'));
+app.use(express.static(__dirname + '/../node_modules'));
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
