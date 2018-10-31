ef7e30256e681288500c1fdd4a0197ade9c99425
seancsutton1 <seancsutton1@gmail.com>
2018-10-31 11:04:26 -0500

    merging

--- server/server.js
+++ server/server.js
@@ -8,10 +8,38 @@ app.use(express.static(__dirname + '/../client'));
 app.use(express.static(__dirname + '/../node_modules'));
 
 app.post('/battle', (req, res) => {
+  const config = {
+    headers: {
+      'TRN-API-Key': '01587c67-4b71-49f8-aeac-eaae6f83ad90',
+    }
+  }
+  axios.get(` https://battlefieldtracker.com/bf1/api/Stats/CareerForOwnedGames?platform=${plat}&displayName=${name}`, config)
+    .then((response) => {
+      console.log(response.data)
+      res.send(response.data);
+    }).catch((err) => {
+      console.log(err);
+      res.send(err);
+    });
+ 
 
 });
 
 app.post('/cod', (req, res) => {
+  
+  const config = {
+    headers: {
+      'TRN-API-Key': '01587c67-4b71-49f8-aeac-eaae6f83ad90',
+    }
+  }
+  axios.get(`https://cod-api.tracker.gg/v1/standard/bo4/profile/${plat}/${name}`, config)
+    .then((response) => {
+      console.log(response.data)
+      res.send(response.data);
+    }).catch((err) => {
+      console.log(err);
+      res.send(err);
+    });
 
 });
 
@@ -21,7 +49,7 @@ app.post('/fort', (req, res) => {
       'TRN-API-Key': '01587c67-4b71-49f8-aeac-eaae6f83ad90',
     }
   }
-  axios.get('https://api.fortnitetracker.com/v1/profile/psn/bout2nut', config)
+  axios.get(`https://api.fortnitetracker.com/v1/profile/${plat}/${name}`, config)
     .then((response) => {
       console.log(response.data)
       res.send(response.data);
