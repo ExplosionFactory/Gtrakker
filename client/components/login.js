583379f4883a95f3200dcb6727f1efa8590d7667
Ethan Hess <erhess0011@gmail.com>
2018-11-02 12:12:07 -0500

    added style changes to login and signup pages

--- client/components/login.js
+++ client/components/login.js
@@ -2,15 +2,6 @@ angular.module('gtrak')
   .component('login', {
     bindings: {},
     templateUrl: 'templates/login.html',
-<<<<<<< HEAD
-  }).controller('loginCtrl',function(){
-    // this.giveCookie = (cname, cvalue, exdays) => {
-    //   const d = new Date();
-    //   d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    //   var expires = "expires="+ d.toUTCString();
-    //   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-    // };
-=======
   }).controller('loginCtrl', ($http, $scope) => {
     $scope.submit = () => {
       $http({
@@ -22,5 +13,4 @@ angular.module('gtrak')
         },
       });
     };
->>>>>>> 03a193371416cf521d0eda276bf06ee25efb22ed
   });
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const session = require('express-session')
+const session = require('express-session');
+
 const app = express();
 const port = 3000;
 const axios = require('axios');
