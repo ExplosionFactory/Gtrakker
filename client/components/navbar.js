d21c9267394d73f931b12d09ce9353992fe06074
seancsutton1 <seancsutton1@gmail.com>
2018-11-05 11:44:46 -0600

    pulling forum code

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,13 +1,11 @@
-/* eslint-disable prefer-arrow-callback */
-/* eslint-disable func-names */
-// eslint-disable-next-line no-undef
+
 angular.module('gtrak')
   .component('navbar', {
     bindings: {
       search: '<',
     },
     templateUrl: 'templates/navbar.html',
-  }).controller('navCtrl', function ($scope, $http, $location) {
+  }).controller('navCtrl',($scope, $http, $location) => {
     $scope.imageAddress = '../../images/gtrakker_logo.png';
     $scope.myValue = false;
     $scope.account = () => {
