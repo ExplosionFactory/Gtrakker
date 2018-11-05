fc7c29fe332d9537af212afdd8fa23132854a903
Ethan Hess <erhess0011@gmail.com>
2018-11-05 12:44:39 -0600

    forums working

--- client/components/forum.js
+++ client/components/forum.js
@@ -4,18 +4,19 @@ angular.module('gtrak')
   .component('forum', {
     bindings: {
     },
-    templateUrl: 'templates/codforum.html',
+    templateUrl: 'templates/forum.html',
   }).controller('forumCtrl', function ($scope, $location, $http) {
-    this.data = {}
+    $scope.data = {};
     $http.get('/forum').then((messages) => {
-      this.data = messages.data;
+      $scope.data.messages = messages.data;
     });
     $scope.submit = () => {
-      $http.post('/forum',{mess:$scope.message}).then(() =>{
-        console.log('posted')
-      })
+      $http.post('/forum', { mess: $scope.message }).then(() => {
+        console.log('posted');
+      });
     };
     $scope.back = () => {
       $location.path('/app');
     };
+    console.log($scope.data);
   });
