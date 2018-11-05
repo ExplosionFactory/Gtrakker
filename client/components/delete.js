25bd3ebc2c83e6294564018c3c480fc7eac0311e
Ethan Hess <erhess0011@gmail.com>
2018-11-05 09:37:12 -0600

    delete go back button added

--- client/components/delete.js
+++ client/components/delete.js
@@ -1,13 +1,19 @@
+/* eslint-disable func-names */
+/* eslint-disable prefer-arrow-callback */
 angular.module('gtrak')
   .component('delete', {
     bindings: {
     },
     templateUrl: 'templates/delete.html',
-  }).controller('deleteCtrl', function ($http, $location, $scope){
-    $scope.delete = () => {
-    console.log("delete")
-    $http.get('/delete').then(() => {
-      $location.path('/splash');
-    });
+  }).controller('deleteCtrl', function ($http, $location, $scope) {
+    $scope.back = () => {
+      $location.path('/app');
     };
-    });
+
+    $scope.delete = () => {
+      console.log('delete');
+      $http.get('/delete').then(() => {
+        $location.path('/splash');
+      });
+    };
+  });
